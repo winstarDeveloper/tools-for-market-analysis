b7da3b0131bf36178f3546eed9a2b30eafdd1991
Prayush <prayushkale@gmail.com>
2020-07-25 23:30:21 +0530

    Removed Flaticon

--- src/index.js
+++ src/index.js
@@ -16,8 +16,8 @@ import "./css/styles.css";
 import "./_metronic/_assets/plugins/keenthemes-icons/font/ki.css";
 import "socicon/css/socicon.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import "./_metronic/_assets/plugins/flaticon/flaticon.css";
-import "./_metronic/_assets/plugins/flaticon2/flaticon.css";
+// import "./_metronic/_assets/plugins/flaticon/flaticon.css";
+// import "./_metronic/_assets/plugins/flaticon2/flaticon.css";
 // Datepicker
 import "react-datepicker/dist/react-datepicker.css";
 import {
