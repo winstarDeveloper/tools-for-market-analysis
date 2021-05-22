5df0701ad179dcde857e0407fa058f5333e589c9
winstarDeveloper <prayushkale@gmail.com>
2021-05-22 10:06:18 +0530

    remove REDUX store

--- src/index.js
+++ src/index.js
@@ -34,20 +34,6 @@ import {MetronicI18nProvider} from "./_metronic/i18n";
  * @see https://facebook.github.io/create-react-app/docs/using-the-public-folder
  */
 const { PUBLIC_URL } = process.env;
-
-/**
- * Creates `axios-mock-adapter` instance for provided `axios` instance, add
- * basic Metronic mocks and returns it.
- *
- * @see https://github.com/ctimmerm/axios-mock-adapter
- */
-/* const mock = */ _redux.mockAxios(axios);
-
-/**
- * Inject metronic interceptors for axios.
- *
- * @see https://github.com/axios/axios#interceptors
- */
 _redux.setupAxios(axios, store);
 
 ReactDOM.render(
